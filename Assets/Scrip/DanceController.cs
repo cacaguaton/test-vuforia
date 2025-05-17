6adda45ed7f064b28a800dbf605bbfd7e347e448
cacaguaton <fernando.martinez@uteca.edu.mx>
2025-05-16 18:36:38 -0600

    Arreglo de botones

---
+++ Assets/Scrip/DanceController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DanceController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _onSelectDance;
+    [SerializeField]
+    private UnityEvent _onDanceSelected;
+
+    public void ActivateSelectDance()
+    {
+        _onSelectDance?.Invoke();
+    }
+
+    public void OnSelectedDance()
+    {
+       _onDanceSelected?.Invoke();
+    }
+}
